e9c3e4375fafd7d34c963383787827078fc99a71
shams1987 <shamsjshaikh@gmail.com>
2023-09-08 12:03:02 -0500

    completed: markdown, entry page, index page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render
+import markdown
 
 from . import util
 
+def md_to_html(title):
+    content = util.get_entry(title)
+    markdowner = markdown.Markdown()
+    if content == None:
+        return None
+    else:
+        return markdowner.convert(content)
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def entry(request, title):
+    content = md_to_html(title)
+    if content:
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "content": content
+        })
+    else:
+        return render(request, "encyclopedia/error.html", {
+            "message": "This entry does not exist"
+        })
+    
+
+def search(request):
+    return
 
