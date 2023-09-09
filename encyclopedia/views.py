38d405e80e17153119794dbe96476dd2a66faff9
shams1987 <shamsjshaikh@gmail.com>
2023-09-09 16:23:11 -0500

    comments on view page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -4,19 +4,24 @@ import random
 
 from . import util
 
+# function to change .md to .html
 def md_to_html(title):
+    # get content
     content = util.get_entry(title)
     markdowner = markdown.Markdown()
-    if content == None:
-        return None
-    else:
+    # convert if there is a content
+    if content:
         return markdowner.convert(content)
+    else:
+        return None
 
+# read all entries
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+# read one entry
 def entry(request, title):
     content = md_to_html(title)
     if content:
@@ -29,7 +34,7 @@ def entry(request, title):
             "message": "This entry does not exist"
         })
     
-
+# search for an entry
 def search(request):
     if request.method == "POST":
         title_search =request.POST['q']
@@ -39,31 +44,43 @@ def search(request):
             "title": title_search,
             "content": content
         })
+        # get entry list
         entries = util.list_entries()
+        # array for search result
         search_result = []
+        # loop through entries
         for entry in entries:
+            # lower case search item is a subset of lower case entry
             if title_search.lower() in entry.lower():
+                # then eppend to array
                 search_result.append(entry)
 
+        # there is result after searching
         if len(search_result) > 0:
             return render(request, "encyclopedia/search.html", {
             "search_result": search_result
             })
+        # no result after searching
         else:
             return render(request, "encyclopedia/error.html", {
             "message": "This entry does not exist"
         })
 
+# create a new entry
 def new_page(request):
+    # go to New Page if link "create new page" is clicked
     if request.method == "GET":
         return render(request, "encyclopedia/new.html")
+    # POST entry
     if request.method == "POST":
         title = request.POST['title']
         content = request.POST['content'] 
+        # new title already exists => send error message
         if title in util.list_entries():
             return render(request, "encyclopedia/error.html", {
                 "message": "Title and content already exists"
             })
+        # new title does not exist => save => display on entry page
         else:
             util.save_entry(title, content)
             new_content = md_to_html(title)
@@ -71,7 +88,9 @@ def new_page(request):
                 "title": title,
                 "content": new_content
             })
-                
+
+# update an entry
+# step 1: display edit form with previous entry
 def edit(request):
     if request.method == "POST":
         title = request.POST['title_entry']
@@ -80,7 +99,8 @@ def edit(request):
             "title": title,
             "content": content
         })
-    
+
+# step 2: save the update
 def save_edit(request):
     if request.method == "POST":
         title = request.POST['title']
